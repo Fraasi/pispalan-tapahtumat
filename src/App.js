ece5c0f46418875b65d21f561b5cddcc0a0f2f27
fraasi <fraasi.gh@gmail.com>
2025-03-20 23:47:07 +0200

    fix: useEffect hook warnings

--- src/App.js
+++ src/App.js
@@ -17,35 +17,35 @@ function App() {
   const [isDarkMode, setDarkMode] = useState(false)
   const [showOnlyPispalaVenues, setShowOnlyPispalaVenues] = useState(true)
 
-  function sortDataToState(data) {
-    const { events_data, map_data, data_updated } = data
-    // sort + move hietis last
-    const hietisIndex = events_data.findIndex(el => el.name === 'Hiedanranta')
-    const hietis = events_data.splice(hietisIndex, 1)[0]
-    const sortedEvents = events_data.sort((first, second) => first.name < second.name ? -1 : 1)
-    sortedEvents.push(hietis)
-    if (process.env.NODE_ENV !== "production") console.log('data:', new Date(data_updated), data)
-    setEvents(() => sortedEvents)
-    setUpdateDate(new Date(data_updated).toLocaleString('FI-fi'))
-    window.map_data = map_data // only way to pass data to LMap, 'cos of the way leaflet works, no props no context :(
-  }
-
-  function getDbData() {
-    fetchData()
-      .then(data => {
-        sortDataToState(data)
-        localStorage.setItem('local_data', JSON.stringify(data))
-      }).catch(err => {
-        console.error('Data fetch error:', err)
-        if (err.toString().includes('TRANSPORT_ERROR')) {
-          setErrorMsg(() => 'Hupsista. Ei saatu yhteyttä tietokantaan. Tarkista nettiyhteytesi ja kokeile ladata sivu uudestaan.')
-        } else {
-          setErrorMsg(() => err.toString())
-        }
-      })
-  }
-
   useEffect(() => {
+    function sortDataToState(data) {
+      const { events_data, map_data, data_updated } = data
+      // sort + move hietis last
+      const hietisIndex = events_data.findIndex(el => el.name === 'Hiedanranta')
+      const hietis = events_data.splice(hietisIndex, 1)[0]
+      const sortedEvents = events_data.sort((first, second) => first.name < second.name ? -1 : 1)
+      sortedEvents.push(hietis)
+      if (process.env.NODE_ENV !== "production") console.log('data:', new Date(data_updated), data)
+      setEvents(() => sortedEvents)
+      setUpdateDate(new Date(data_updated).toLocaleString('FI-fi'))
+      window.map_data = map_data // only way to pass data to LMap, 'cos of the way leaflet works, no props no context :(
+    }
+
+    function getDbData() {
+      fetchData()
+        .then(data => {
+          sortDataToState(data)
+          localStorage.setItem('local_data', JSON.stringify(data))
+        }).catch(err => {
+          console.error('Data fetch error:', err)
+          if (err.toString().includes('TRANSPORT_ERROR')) {
+            setErrorMsg(() => 'Hupsista. Ei saatu yhteyttä tietokantaan. Tarkista nettiyhteytesi ja kokeile ladata sivu uudestaan.')
+          } else {
+            setErrorMsg(() => err.toString())
+          }
+        })
+    }
+
     const localDarkMode = localStorage.getItem('dark_mode')
     const preferDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
     if (localDarkMode === 'false' || (localDarkMode === null && preferDarkMode === false)) {
@@ -59,16 +59,13 @@ function App() {
     // check for localStorage & data_updated + 1 day less than now
     const local_data = JSON.parse(localStorage.getItem('local_data')) // JSON.parse(null) returns also null
     if ( !local_data ) {
-      // no localstorage, get new data
-      getDbData()
+      getDbData() // no localstorage, get new data
     } else {
       const local_data_updated = new Date(local_data.data_updated)
-      if (local_data_updated.setDate(local_data_updated.getDate() + 1) < new Date) {
-        // local storage out of date, fetch new data
-        getDbData()
+      if (local_data_updated.setDate(local_data_updated.getDate() + 1) < new Date()) {
+        getDbData() // local storage out of date, fetch new data
       } else {
-        // use data from localstorage
-        sortDataToState(local_data)
+        sortDataToState(local_data) // use data from localstorage
       }
     }
   }, [])
